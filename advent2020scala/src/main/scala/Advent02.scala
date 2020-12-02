4d1b581aeea63682a1082a10514a9c7f692e3085
Christopher Cerny <cerny.christopher@gmx.at>
2020-12-02 06:35:00 +0100

    reformat using scalafmt

--- advent2020scala/src/main/scala/Advent02.scala
+++ advent2020scala/src/main/scala/Advent02.scala
@@ -6,28 +6,24 @@ object Advent02 {
 
     val regex = raw"(\d+)-(\d+) (.): (.+)".r
 
-    val passwords = inputs.collect {
-      case regex(min, max, char, password) => PasswordPolicy(min.toInt, max.toInt, char.charAt(0)) -> password
+    val passwords = inputs.collect { case regex(min, max, char, password) =>
+      PasswordPolicy(min.toInt, max.toInt, char.charAt(0)) -> password
     }
 
     // count the number of passwords that are valid according to the policy
-    println(
-      passwords.count { case (policy, password) =>
-        val charCount = password.count(_ == policy.char)
+    println(passwords.count { case (policy, password) =>
+      val charCount = password.count(_ == policy.char)
 
-        charCount >= policy.first && charCount <= policy.second
-      }
-    )
+      charCount >= policy.first && charCount <= policy.second
+    })
 
     // oh no, the policy has changed! Exactly one character at the given positions must be equal to the character
     // stored in the password policy
-    println(
-      passwords.count { case (policy, password) =>
-        val firstChar = password.charAt(policy.first - 1)
-        val secondChar = password.charAt(policy.second - 1)
+    println(passwords.count { case (policy, password) =>
+      val firstChar = password.charAt(policy.first - 1)
+      val secondChar = password.charAt(policy.second - 1)
 
-        (firstChar == policy.char) ^ (secondChar == policy.char)
-      }
-    )
+      (firstChar == policy.char) ^ (secondChar == policy.char)
+    })
   }
 }
