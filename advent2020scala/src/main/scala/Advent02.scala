f6db0ef93517f667ed7ac06d51b193c28f794548
Christopher Cerny <cerny.christopher@gmx.at>
2020-12-02 06:26:26 +0100

    day 2: count how many passwords are valid according to a password policy

---
+++ advent2020scala/src/main/scala/Advent02.scala
@@ -0,0 +1,33 @@
+object Advent02 {
+  case class PasswordPolicy(first: Int, second: Int, char: Char)
+
+  def main(args: Array[String]): Unit = {
+    val inputs = Input.byExercise(2)
+
+    val regex = raw"(\d+)-(\d+) (.): (.+)".r
+
+    val passwords = inputs.collect {
+      case regex(min, max, char, password) => PasswordPolicy(min.toInt, max.toInt, char.charAt(0)) -> password
+    }
+
+    // count the number of passwords that are valid according to the policy
+    println(
+      passwords.count { case (policy, password) =>
+        val charCount = password.count(_ == policy.char)
+
+        charCount >= policy.first && charCount <= policy.second
+      }
+    )
+
+    // oh no, the policy has changed! Exactly one character at the given positions must be equal to the character
+    // stored in the password policy
+    println(
+      passwords.count { case (policy, password) =>
+        val firstChar = password.charAt(policy.first - 1)
+        val secondChar = password.charAt(policy.second - 1)
+
+        (firstChar == policy.char) ^ (secondChar == policy.char)
+      }
+    )
+  }
+}
