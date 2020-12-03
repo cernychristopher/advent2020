df2396259f040e139b8ed66362e5fb22b01da504
Christopher Cerny <cerny.christopher@gmx.at>
2020-12-03 20:42:04 +0100

    day 3: how many trees (#) do we hit when we fly through a forest on a certain path?
    It's been a while since I have written a counting loop :-)

---
+++ advent2020scala/src/main/scala/Advent03.scala
@@ -0,0 +1,31 @@
+object Advent03 {
+  def countTrees(inputs: List[String], stepRight: Int, stepDown: Int): Long = {
+    var left: Int = 0
+    var count: Long = 0
+
+    for {
+      i <- stepDown until inputs.length by stepDown
+    } {
+      val line = inputs(i)
+      left += stepRight
+      left %= line.length
+
+      if (line.charAt(left) == '#') count = count + 1
+    }
+
+    count
+  }
+
+  def main(args: Array[String]): Unit = {
+    val inputs = Input.byExercise(3)
+
+    val count11 = countTrees(inputs, 1, 1)
+    val count31 = countTrees(inputs, 3, 1)
+    val count51 = countTrees(inputs, 5, 1)
+    val count71 = countTrees(inputs, 7, 1)
+    val count12 = countTrees(inputs, 1, 2)
+
+    println(s"Soltution1: $count31")
+    println(s"Soltution2: ${count11 * count31 * count51 * count71 * count12}")
+  }
+}
