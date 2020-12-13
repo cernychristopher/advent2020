e71228bc2de97b22c4c04997ac51c4160e50a24d
Christopher Cerny <cerny.christopher@gmx.at>
2020-12-13 10:44:02 +0100

    Some comments to help me understand the solution better tomorrow.

--- advent2020scala/src/main/scala/Advent13.scala
+++ advent2020scala/src/main/scala/Advent13.scala
@@ -12,7 +12,6 @@ object Advent13 {
     }
 
     val waitTime = actualDeparture.map(_ - earliestDeparture)
-
     val bus = actualDeparture.flatMap { departure => busses.find(bus => departure % bus == 0) }
 
     val solution1 = for {
@@ -23,24 +22,35 @@ object Advent13 {
     println(s"Solution1: $solution1")
 
     val allBusses = input
-      .drop(1)
+      .drop(1) // the first line is irrelevant to solution2
       .head
       .split(',')
       .map {
         case "x" => None
         case s   => Option(s.toInt)
       }
-      .zipWithIndex
+      .zipWithIndex // index: this bus should arrive index minutes _after the first bus_
       .collect { case (Some(bus), offset) =>
-        (bus, (10 * bus - offset) % bus)
+        (
+          bus,
+          (bus - offset % bus) % bus // Bus 17 arrives 2 minutes after the first bus === timestamp is in residue class (17 -2) mod 17
+        )
       }
-      .sortBy(_._1)
-      .reverse
+
+    println(allBusses.toList)
 
     val solution2 = allBusses
-      .foldLeft((0L, 1L)) { case ((start, product), (bus, offset)) =>
-        val nextStart = Iterator.iterate(start) { _ + product }.find { _ % bus == offset }.get
-        (nextStart, product * bus)
+      .sortBy(-_._1) // looking at busses that arrive rarely first prunes the search space faster
+      .foldLeft((0L, 1L)) { case ((start, step), (bus, offset)) =>
+        // assumption: all the busses have no common divisors
+
+        // start: the first number that follows the rules for all busses seen so far
+        //   numbers that _also_ follow those rules are 'step' apart
+        //
+        // find the first number that follows the rules so far AND ALSO for this bus
+        val nextStart = Iterator.iterate(start) { _ + step }.find { _ % bus == offset }.get
+
+        (nextStart, step * bus)
       }
       ._1
 
