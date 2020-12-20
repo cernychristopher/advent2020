b22dc84ff379cef8fbad4a2fe685cfb9582cf105
Christopher Cerny <cerny.christopher@gmx.at>
2020-12-20 08:47:54 +0100

    border time

--- advent2020scala/src/main/scala/Advent20.scala
+++ advent2020scala/src/main/scala/Advent20.scala
@@ -1,5 +1,7 @@
 import Input.ListOps
 
+import scala.annotation.tailrec
+
 case class Tile(id: Int, lines: List[String]) {
   lazy val normalBorders: List[String] = List(
     lines.head,
@@ -34,11 +36,16 @@ object Advent20 {
       tile -> adjacentTiles
     }.toMap
 
-    neighborGrid.foreach { case (key, neighbors) => println(s"${key.id}: ${neighbors.map(_.id)}") }
+    //neighborGrid.foreach { case (key, neighbors) => println(s"${key.id}: ${neighbors.map(_.id)}") }
 
-    val cornerGrids = neighborGrid.filter(_._2.length == 2).keys.map(_.id).toList
+    val cornerGrids = neighborGrid.filter(_._2.length == 2).keys.toList
 
-    val solution1 = cornerGrids.map(_.toLong).tapEach(println).product
+    val solution1 = cornerGrids.map(_.id.toLong).product
+
+    val borderTiles =
+      neighborGrid.filter(_._2.length < 4).keys.toList
+
+    computeBorder(cornerGrids.head, borderTiles, neighborGrid).foreach(println)
 
     val solution2 = List(7)
 
@@ -48,5 +55,36 @@ object Advent20 {
 
   def toTile(in: List[String]): Tile = in match {
     case header :: lines => Tile(header.drop("Tile ".length).takeWhile(_.isDigit).toInt, lines)
+  }
+
+  def computeBorder[T](corner: T, borderTiles: List[T], neighborGrid: Map[T, List[T]]): List[T] = {
+    val inBorder = borderTiles.toSet
+
+    println(borderTiles.size)
+
+    @tailrec
+    def go(acc: List[T], remaining: List[T]): List[T] = {
+      val lastPoint = acc.head
+
+      remaining match {
+        case Nil => acc
+        case _ =>
+          val foo = neighborGrid(lastPoint).filter { otherPoint =>
+            inBorder.contains(otherPoint) &&
+            !acc.contains(otherPoint) &&
+            remaining.contains(otherPoint)
+          }
+
+          if (foo.isEmpty) {
+            println("FSCK")
+          }
+
+          val nextPoint = foo.head
+
+          go(nextPoint :: acc, remaining.filterNot(_ == nextPoint))
+      }
+    }
+
+    go(List(corner), borderTiles.filterNot(_ == corner))
   }
 }
